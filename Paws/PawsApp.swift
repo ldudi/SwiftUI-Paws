e6a080d48515ad5ca55f39e5a13778517a2a4e5a
labhesh <labhesh.dudi@gmaechangesns.com>
2025-04-08 23:52:32 +0530

    Initial Commit

---
+++ Paws/PawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawsApp.swift
+//  Paws
+//
+//  Created by Labhesh Dudi on 08/04/25.
+//
+
+import SwiftUI
+
+@main
+struct PawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

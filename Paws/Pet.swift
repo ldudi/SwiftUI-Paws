ac168ce78b79c5426926f0a1c2dfbd8e7eea0b3a
labhesh <labhesh.dudi@gmaechangesns.com>
2025-04-09 17:30:06 +0530

    SwiftData Project Setup

---
+++ Paws/Pet.swift
@@ -0,0 +1,20 @@
+//
+//  Pet.swift
+//  Paws
+//
+//  Created by Labhesh Dudi on 09/04/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Pet {
+    var name: String
+    @Attribute(.externalStorage) var photo: Data?
+    
+    init(name: String, photo: Data? = nil) {
+        self.name = name
+        self.photo = photo
+    }
+}
